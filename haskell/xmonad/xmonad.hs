34f74a09ab767c7228809568efe87b38ab242da8
Stefan Eng <stefaneng13@gmail.com>
2013-11-07 22:44:24 -0800

    Added a template .xmobarrc config file from somewhere. Working .xmobar that needs more customization.

--- haskell/xmonad/xmonad.hs
+++ haskell/xmonad/xmonad.hs
@@ -5,9 +5,17 @@
 --     Xmonad window manager configuration file.
 
 import XMonad
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.Cursor
 
-main = do
-    xmonad $ defaultConfig
-       { modMask = mod4Mask
-       , terminal = "urxvt"
-       }
+main = xmonad =<< xmobar myConfig
+
+myConfig = defaultConfig
+           { modMask    = mod4Mask -- Use windows key for mod
+           , terminal   = "urxvt"
+           , manageHook = manageDocks <+> manageHook defaultConfig
+           , layoutHook = avoidStruts  $ layoutHook defaultConfig           
+           , startupHook = setDefaultCursor xC_left_ptr
+           }
+
