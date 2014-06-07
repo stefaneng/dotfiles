3709e4e1b74f4cb3d2f8b517b647d979a860c1b6
Stefan Eng <stefaneng13@gmail.com>
2014-06-07 16:46:56 -0700

    Switched keybindings

--- emacs/elget-inits/init-smartparens.el
+++ emacs/elget-inits/init-smartparens.el
@@ -5,5 +5,5 @@
 
 (define-key sp-keymap (kbd "C-)") 'sp-forward-slurp-sexp)
 (define-key sp-keymap (kbd "C-(") 'sp-backward-slurp-sexp)
-(define-key sp-keymap (kbd "C-{") 'sp-forward-barf-sexp)
-(define-key sp-keymap (kbd "C-}") 'sp-backward-barf-sexp)
+(define-key sp-keymap (kbd "C-}") 'sp-forward-barf-sexp)
+(define-key sp-keymap (kbd "C-{") 'sp-backward-barf-sexp)
