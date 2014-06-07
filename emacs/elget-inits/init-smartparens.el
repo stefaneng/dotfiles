11fe2f8637329b5e1b06812aea33ba083b4136f4
Stefan Eng <stefaneng13@gmail.com>
2014-06-07 16:41:51 -0700

    Started mapping paredit keybindings in smartparens

--- emacs/elget-inits/init-smartparens.el
+++ emacs/elget-inits/init-smartparens.el
@@ -2,3 +2,8 @@
 
 (smartparens-global-mode)
 (show-smartparens-global-mode)
+
+(define-key sp-keymap (kbd "C-)") 'sp-forward-slurp-sexp)
+(define-key sp-keymap (kbd "C-(") 'sp-backward-slurp-sexp)
+(define-key sp-keymap (kbd "C-{") 'sp-forward-barf-sexp)
+(define-key sp-keymap (kbd "C-}") 'sp-backward-barf-sexp)
