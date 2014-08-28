1c9f8c9614129e268d135e3095d1daa14e0395f5
Stefan Eng <stefaneng13@gmail.com>
2014-08-27 19:03:31 -0700

    Some org mode settings

--- emacs/elget-inits/init-org-mode.el
+++ emacs/elget-inits/init-org-mode.el
@@ -3,3 +3,16 @@
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
+
+;; Keep track of when a certain TODO was finished
+(setq org-log-done 'time)
+
+;; Set agenda files
+(setq org-agenda-files (list "~/org/agenda.org"))
+
+;; Default notes file
+(setq org-default-notes-file "~/org/notes.org")
+
+;; Save clock history across emacs sessions
+(setq org-clock-persist 'history)
+(org-clock-persistence-insinuate)
